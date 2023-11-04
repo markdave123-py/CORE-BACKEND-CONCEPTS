8d4544877f856c3b71d72c2525598644ea16b2d7
markdave123-py <nwaekwudavid@gmail.com>
2023-11-04 22:10:50 +0100

    networking

--- src/simple.sender.js
+++ src/simple.sender.js
@@ -1,5 +1,8 @@
 const net = require('net');
 
 const socket = net.createConnection({host: "127.0.0.1", port: 4081}, () => {
-    socket.write("Asimple message coming from a client");
+    const buff = Buffer.alloc(2)
+    buff[0] = 10;
+    buff[1] = 12;
+    socket.write(buff);
 });

1ac3eb32766c5ac42b4021973beaa6cc150bbcbe
markdave123-py <nwaekwudavid@gmail.com>
2023-11-11 21:42:21 +0100

    events

--- src/Event-Emitter-core/event.js
+++ src/Event-Emitter-core/event.js
@@ -1,10 +1,24 @@
 const eventEmitter = require('events');
 
 class Event extends eventEmitter {
-    constructor() {
-        super();
-    }
+    // constructor() {
+    //     super();
+    // }
 }
 
-const event = new Event();
+const myEvent = new Event();
 
+myEvent.on("foo", ()=>{
+    console.log("event occured")
+})
+myEvent.on("foo", ()=>{
+    console.log("event occured second time")
+})
+
+myEvent.on("foo", (x)=>{
+    console.log("event occured bar 1")
+    console.log(x)
+})
+
+myEvent.emit("foo")
+myEvent.emit("foo", "some text")
